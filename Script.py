b9a75ed7f9fceb30c6bcf8858fbceef69faa1593
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-12-30 19:11:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Monster_Mind_Moviez">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Monster_Mind_Moviez">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
